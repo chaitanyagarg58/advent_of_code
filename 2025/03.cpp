ad28dd813cb393ff482b96cbe6edcf7be5cae42c
chaitanyagarg <chaitanya.gargsg@gmail.com>
2025-12-03 18:32:55 +0530

    Optimized 2025/03

--- 2025/03.cpp
+++ 2025/03.cpp
@@ -1,28 +1,27 @@
 #include <iostream>
-
-int find_next_digit(const std::string& bank, int start_idx, int num_digits_remaining)
-{
-    if (bank.length() - start_idx < num_digits_remaining) throw std::runtime_error(std::string("Num digits remaining is less than num available digits"));
-    
-    int idx[10];
-    for (int& i: idx) i = -1;
-
-    for (int i = bank.length() - num_digits_remaining; i >= start_idx; i--) idx[bank[i] - '0'] = i;
-    for (int i = 9; i >= 0; i--) if (idx[i] != -1) return idx[i];
-
-    throw std::runtime_error(std::string("Should Never reach here"));
-}
+#include <queue>
 
 long long get_max_num(const std::string& bank, int num_digits)
 {
     long long num = 0;
+    std::queue<int> idx[10];
+    for (int i = 0; i < bank.length(); i++) idx[bank[i] - '0'].push(i);
+
     int start_idx = 0;
     while (num_digits > 0)
     {
-        int idx = find_next_digit(bank, start_idx, num_digits);
-        num = num * 10 + (bank[idx] - '0');
-        start_idx = idx + 1;
-        num_digits--;
+        for (int i = 9; i >= 0; i--)
+        {
+            while (idx[i].size() && idx[i].front() < start_idx) idx[i].pop();
+            if (idx[i].size() && idx[i].front() <= bank.length() - num_digits)
+            {
+                num = num * 10 + (bank[idx[i].front()] - '0');
+                start_idx = idx[i].front() + 1;
+                idx[i].pop();
+                num_digits--;
+                break;
+            }
+        }
     }
     return num;
 }
